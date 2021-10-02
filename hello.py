7f5fd568bdf06cd5d285484a4f12cf01dc047b24
josh-arrabaca <72542731+josh-arrabaca@users.noreply.github.com>
2021-10-02 11:15:42 +0800

    Fixed indentations again with 4 spaces

--- hello.py
+++ hello.py
@@ -3,10 +3,10 @@ file docstring for pylint
 """
 
 def add(x_var, y_var):
-   """
-   function docstring for pylint
-   """
-   return x_var + y_var
+    """
+    function docstring for pylint
+    """
+    return x_var + y_var
 
 
 RESULT = add(1, 2)
