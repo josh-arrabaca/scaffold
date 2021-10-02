bf537c1d7187f8932186ae55dcdcf11f10db7a16
josh-arrabaca <72542731+josh-arrabaca@users.noreply.github.com>
2021-10-02 11:14:03 +0800

    fixed indentations

--- hello.py
+++ hello.py
@@ -4,9 +4,9 @@ file docstring for pylint
 
 def add(x_var, y_var):
    """
-    function docstring for pylint
-    """
-    return x_var + y_var
+   function docstring for pylint
+   """
+   return x_var + y_var
 
 
 RESULT = add(1, 2)
